57a99081a22d2aa7931e2723f05c6dcd2cfcc31f
Valerii Bubenshchikov <valerii_bubenshchikov@epam.com>
2024-07-31 23:02:49 +0400

    eslint unused imports

--- packages/engine/eslint.config.mjs
+++ packages/engine/eslint.config.mjs
@@ -1,6 +1,7 @@
 import pluginJs from '@eslint/js';
 
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import unusedImports from 'eslint-plugin-unused-imports';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -13,6 +14,29 @@ export default [
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
   {
+    plugins: {
+      'unused-imports': unusedImports
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_'
+        }
+      ]
+    }
+  },
+
+  {
     ignores: ['**/mock-repo/**/*', '**/dist/**/*']
+  },
+  {
+    rules: {}
   }
 ];
